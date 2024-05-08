a872b71b56fc63b60ddab9e6f45552c91164755b
perodriguez1630 <139923454+perodriguez1630@users.noreply.github.com>
2024-05-08 15:36:24 -0400

    utpdate

--- deploy-script.sh
+++ deploy-script.sh
@@ -25,18 +25,27 @@
 INSTANCE_NAME="instance-gce-test"
 ZONE="us-central1-b"
 PROJECT_ID="rosy-acolyte-412215"
-SCRIPT_PATH="/home/joshua163011/"  # Ruta al archivo .sh dentro de la instancia
+SCRIPT_PATH="/home/joshua163011/test.sh"  # Ruta al archivo .sh dentro de la instancia
 
 # Verificar si la instancia existe
 if gcloud compute instances describe $INSTANCE_NAME --zone=$ZONE --project=$PROJECT_ID &>/dev/null; then
-  echo "La instancia ya existe, ejecutando el archivo .sh..."
-  # Ejecutar el archivo .sh dentro de la instancia existente usando SSH
-  gcloud compute ssh $INSTANCE_NAME --zone=$ZONE --project=$PROJECT_ID --command="bash -s" < $SCRIPT_PATH
+    echo "La instancia existe, ejecutando el archivo .sh..."
+    # Conectarse a la instancia y ejecutar el script
+    gcloud compute ssh $INSTANCE_NAME --zone=$ZONE --project=$PROJECT_ID --command="bash $SCRIPT_PATH"
 else
-  echo "La instancia no existe, creando la instancia y ejecutando el archivo .sh..."
-  # Crear la instancia y ejecutar el archivo .sh
-  gcloud compute instances create $INSTANCE_NAME \
-    --zone=$ZONE \
-    --metadata-from-file startup-script=$SCRIPT_PATH
+    echo "La instancia no existe."
 fi
+
+# Verificar si la instancia existe
+# if gcloud compute instances describe $INSTANCE_NAME --zone=$ZONE --project=$PROJECT_ID &>/dev/null; then
+#   echo "La instancia ya existe, ejecutando el archivo .sh..."
+#   # Ejecutar el archivo .sh dentro de la instancia existente usando SSH
+#   gcloud compute ssh $INSTANCE_NAME --zone=$ZONE --project=$PROJECT_ID --command="bash -s" < $SCRIPT_PATH
+# else
+#   echo "La instancia no existe, creando la instancia y ejecutando el archivo .sh..."
+#   # Crear la instancia y ejecutar el archivo .sh
+#   gcloud compute instances create $INSTANCE_NAME \
+#     --zone=$ZONE \
+#     --metadata-from-file startup-script=$SCRIPT_PATH
+# fi
 
